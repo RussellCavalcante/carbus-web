f04e7f5bcae7b6a123d5684277db61e0b17cbfd4
Russell Cavalcante <russell.cavalcante.2882@gmail.com>
2024-07-02 18:51:33 -0300

    realizado reformulacao da carbus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { HistoryComponent } from './history/history.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    HeaderComponent,
+    HeroComponent,
+    HistoryComponent,
+    ProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  products = [
+    { image: 'assets/PRODUTOS/01.jpeg', title: 'PONTEIRAS' },
+    { image: 'assets/PRODUTOS/02.jpeg', title: 'LANTERNAS' },
+    { image: 'assets/PRODUTOS/03-removebg-preview.png', title: 'Faróis' },
+    { image: 'assets/PRODUTOS/04.jpeg', title: 'PISTÕES' },
+    { image: 'assets/PRODUTOS/05.jpeg', title: 'PÁRA-BRISA E VIDRO' },
+    { image: 'assets/PRODUTOS/06.jpeg', title: 'LIMPADORES DE PÁRA-BRISA' },
+    { image: 'assets/PRODUTOS/07.jpeg', title: 'TECIDOS E TAPETES' },
+    { image: 'assets/PRODUTOS/08.jpeg', title: 'RETROVISORES' },
+    { image: 'assets/PRODUTOS/09.jpeg', title: 'PERFIL ALUMÍNIO E PVC' },
+    { image: 'assets/PRODUTOS/10.jpeg', title: 'PARA-CHOQUE' },
+    { image: 'assets/PRODUTOS/11.jpeg', title: 'CHAPA' },
+    { image: 'assets/PRODUTOS/12.jpeg', title: 'Faróis' },
+  ];
+
+  ngOnInit(): void {
+  }
+
+}
