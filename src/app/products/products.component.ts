7598080f95e871cd2082ee56d16ee097da0ddae7
Russell Cavalcante <russell.cavalcante.2882@gmail.com>
2024-07-02 21:02:50 -0300

    new prodruct case, add link para redirecionamento no wpp !

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -12,7 +12,7 @@ export class ProductsComponent implements OnInit {
   products = [
     { image: 'assets/PRODUTOS/01.jpeg', title: 'PONTEIRAS' },
     { image: 'assets/PRODUTOS/02.jpeg', title: 'LANTERNAS' },
-    { image: 'assets/PRODUTOS/03-removebg-preview.png', title: 'Faróis' },
+    { image: 'assets/PRODUTOS/03-removebg-preview.png', title: 'Borracha' },
     { image: 'assets/PRODUTOS/04.jpeg', title: 'PISTÕES' },
     { image: 'assets/PRODUTOS/05.jpeg', title: 'PÁRA-BRISA E VIDRO' },
     { image: 'assets/PRODUTOS/06.jpeg', title: 'LIMPADORES DE PÁRA-BRISA' },
@@ -21,8 +21,12 @@ export class ProductsComponent implements OnInit {
     { image: 'assets/PRODUTOS/09.jpeg', title: 'PERFIL ALUMÍNIO E PVC' },
     { image: 'assets/PRODUTOS/10.jpeg', title: 'PARA-CHOQUE' },
     { image: 'assets/PRODUTOS/11.jpeg', title: 'CHAPA' },
-    { image: 'assets/PRODUTOS/12.jpeg', title: 'Faróis' },
+    { image: 'assets/PRODUTOS/12.jpeg', title: 'Caneleta' },
   ];
+
+  openWhatsApp(): void {
+    window.open('https://wa.me/5565993557958', '_blank');
+  }
 
   ngOnInit(): void {
   }
